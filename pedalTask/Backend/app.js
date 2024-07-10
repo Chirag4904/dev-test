c6e8802b633ed6085b9a2fc2d084ee2c66467a65
chiggy69 <chiragaggarwal69@gmail.com>
2024-07-10 20:45:03 +0530

    afdfsf

--- pedalTask/Backend/app.js
+++ pedalTask/Backend/app.js
@@ -11,14 +11,17 @@ dotEnv.config();
 const PORT = 3000;
 
 app.use(express.json());
+app.get("/", (req, res) => {
+	res.send(200).json({ message: "Welcome" });
+});
 app.use("/api", taskRoutes);
 
 sequelize
-  .sync({ force: true })
-  .then(() => {
-    console.log("Database synced successfully");
-    app.listen(3000, () => console.log("Server ready on port 3000."));
-  })
-  .catch((error) => {
-    console.error("Unable to sync database:", error);
-  });
+	.sync({ force: false })
+	.then(() => {
+		console.log("Database synced successfully");
+		app.listen(3000, () => console.log("Server ready on port 3000."));
+	})
+	.catch((error) => {
+		console.error("Unable to sync database:", error);
+	});
